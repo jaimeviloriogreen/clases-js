6236bcac38cc53db465d9edb07a9d775ea30b95b
jaimeviloriogreen <jaimeviloriogreen@hotmail.com>
2023-08-30 10:04:54 -0400

    edit done

--- src/trabajo-final/app.js
+++ src/trabajo-final/app.js
@@ -3,10 +3,12 @@ import { f } from "./modules/formatearMoneda.js";
 //? Variables
 const agregar = document.querySelector(".products__btn");
 const btnCancelar = document.querySelector(".form__btn--cancelar");
+const btnInsertar = document.querySelector(".form__btn--insertar");
 const productForm = document.querySelector(".products__form");
 const form = document.querySelector(".form");
 const containerTable = document.querySelector(".products__container--table");
 const containerIcon = document.querySelector(".products__container--icon");
+let idEditar;
 
 containerTable.addEventListener("click", doAction);
 
@@ -24,13 +26,16 @@ if(form) form.addEventListener("submit", capturarProductosForm);
 
 
 //? Funciones
-function mostrarAgregarForm(e){
-    // e.stopPropagation();
+function mostrarAgregarForm(){
     if(form.classList.contains("form--animationHide")){
         form.classList.remove("form--animationHide");
     }   
 
     productForm.classList.remove("product__form--display");
+
+    btnInsertar.dataset.do="insertar";
+    btnInsertar.textContent = "Insertar";
+
     form.classList.add("form--animationShow");
     form.producto.focus();
 }
@@ -52,15 +57,21 @@ function capturarProductosForm(e){
     const precio = form.precio.valueAsNumber;
     const cantidad = form.cantidad.valueAsNumber;
 
-    ++id;
-    const item = getProducto(id, producto, precio, cantidad);
+    if(btnInsertar.dataset.do === "insertar"){
+        ++id;
+        const item = getProducto(id, producto, precio, cantidad);
 
-    productos.push(item)
-    
-    // Se muestra la tabla y se renderiza el table body si hay un valor en arreglo producto
-    mostrarTabla();
-    renderizarBodyTable(item);
-    
+        productos.push(item)
+        // Se muestra la tabla y se renderiza el table body si hay un valor en arreglo producto
+        mostrarTabla();
+        renderizarBodyTable(item);
+    }
+
+    if(btnInsertar.dataset.do === "actualizar"){
+        editarItemProducto(idEditar, producto, precio, cantidad);
+        editarProductoTableHTML(idEditar,producto, precio, cantidad);
+    }
+
     // Cacula factura
     const {subTotal, itbis, total} = calcularFactura();
     renderizarFactura(subTotal, itbis, total);
@@ -152,29 +163,52 @@ function doAction(e){
     if(e.target.classList.contains("table__edit") || 
     e.target.classList.contains("table__icon--edit")){
         const id = e.target.dataset.id || e.target.parentElement.dataset.id;
-        console.log(id);
+        mostrarAgregarForm();
+        
+        btnInsertar.dataset.do="actualizar";
+        idEditar = id;
+        btnInsertar.textContent = "Actualizar";
+        const {producto, precio, cantidad} = itemValuesEditar(id)
+        
+        form.producto.value = producto;
+        form.precio.value = precio;
+        form.cantidad.value = cantidad;
     }
 }
 function eliminarItemTable(id){
     const trs = document.querySelector(".table__tbody").childNodes;
-        trs.forEach((tr) => {
-            if(tr.dataset.id == id){
-                tr.remove();
-            }
-        });
+    trs.forEach((tr) => {
+        if(tr.dataset.id == id){
+            tr.remove();
+        }
+    });
 };
+function itemValuesEditar(id){
+    let result = productos.find( p => p.id == id);
+    return result;
+}
 function eliminarItemProducto(id){
     const idx = productos.findIndex(p => p.id == id);
     productos.splice(idx, 1);
     
     const {subTotal, itbis, total} = calcularFactura();
     renderizarFactura(subTotal, itbis, total);
-    console.log(productos);
 
     if(productos.length <= 0){
         containerTable.classList.add("products__container--display");
         containerIcon.classList.remove("products__container--display");
     }
 }
-
+function editarItemProducto(id, producto, precio, cantidad){
+    const idx = productos.findIndex(p => p.id == id);
+    productos[idx].producto = producto;
+    productos[idx].precio = precio;
+    productos[idx].cantidad = cantidad;
+}
+function editarProductoTableHTML(id, producto, precio, cantidad){
+    const trs = document.querySelector(`.table__tbody > tr[data-id="${id}"]`);
+    trs.children[0].textContent = producto;
+    trs.children[1].textContent = f.format(precio);
+    trs.children[2].textContent = cantidad;
+}
 
